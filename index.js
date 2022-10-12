4cb55072c384a0f21002a673eaffde821e6aeb41
Diana Vu <aznalive@hotmail.com>
2022-10-12 00:17:10 -0400

    created the prompts for the rest of the team, added team array, write file function

--- index.js
+++ index.js
@@ -20,3 +20,58 @@ const addManager = () => {
     teamArray.push(manager)
   })
 }
+//adds employees
+const addEmployee = () => {
+  console.log(`
+    =================
+    Adding employees to the team
+    =================
+    `)
+
+  return inquirer.prompt(employeeChoices).then(employeeData => {
+    let {
+      name,
+      id,
+      email,
+      role,
+      github,
+      school,
+      confirmAddEmployee
+    } = employeeData
+
+    let employee =
+      role === 'Engineer'
+        ? new Engineer(name, id, email, github)
+        : new Intern(name, id, email, school)
+
+    teamArray.push(employee)
+
+    if (confirmAddEmployee) {
+      return addEmployee(teamArray)
+    }
+
+    return teamArray
+  })
+}
+
+//writes html file
+const writeFile = data => {
+  fs.writeFile('./dist/index.html', data, err => {
+    // throws out an error if it catches one
+    if (err) {
+      console.log(err)
+      return
+      // success, html is created
+    } else {
+      console.log(
+        'Success! Your team has been generated, open up the HTML to check it out.'
+      )
+    }
+  })
+}
+
+addManager()
+  .then(addEmployee)
+  .then(teamArray => HTML(teamArray))
+  .then(pageHTML => writeFile(pageHTML))
+  .catch(err => console.log(err))
