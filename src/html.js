39ebf29f99ad37906b63766ff10eca6346351a8f
Diana Vu <aznalive@hotmail.com>
2022-10-12 16:44:14 -0400

    re adjusted file structure, did another test to make sure everything ran correctly

--- src/html.js
+++ src/html.js
@@ -1,3 +1,57 @@
+// this creates the teampage with all the info passed in
+const createTeamPage = function (teamCards) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Amazing Team Profile</title>
+      <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Peralta&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <header>
+          <nav class="navbar" id="nav">
+              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Amazing Team Profile</span>
+          </nav>
+      </header>
+      <main>
+          <section class="container">
+              <div class="row justify-content-center" id="team-cards">
+                  <!--Cards for each profile created-->
+                  ${teamCards}
+              </div>
+          </section>
+      </main>
+  </body>
+  </html>
+`
+};
+
+displayTeamPage = data => {
+  const cardArray = []
+  for (let i = 0; i < data.length; i++) {
+    let team = data[i]
+    let role = team.getRole()
+    if (role === 'Manager') {
+      const managerCard = ManagerCard(team)
+      cardArray.push(managerCard)
+    } else if (role === 'Engineer') {
+      const engineerCard = EngineerCard(team)
+      cardArray.push(engineerCard)
+    } else if (role === 'Intern') {
+      const internCard = InternCard(team)
+      cardArray.push(internCard)
+    }
+  }
+  const teamCards = cardArray.join('')
+  return createTeamPage(teamCards)
+}
+
 // manager profile
 const ManagerCard = function(manager) {
     const {name, id, email, officeNumber} = manager;
@@ -54,61 +108,6 @@ const InternCard = function(intern) {
     </div>
 </section>
     `
-};
-
-displayTeamPage = data => {
-  const cardArray = [];
-  for (let i = 0; i < data.length; i++) {
-    let team = data[i];
-    let role = team.getRole();
-    if (role === 'Manager') {
-      const managerCard = ManagerCard(team);
-      cardArray.push(managerCard);
-    }
-    else if (role === 'Engineer') {
-      const engineerCard = EngineerCard(team);
-      cardArray.push(engineerCard);
-    }
-    else if (role === 'Intern') {
-      const internCard = InternCard(team);
-      cardArray.push(internCard);
-    }
-  }
-  const teamCards = cardArray.join('');
-  return createTeamPage(teamCards);
-};
-// this creates the teampage with all the info passed in
-const createTeamPage = function(teamCards) {
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Amazing Team Profile</title>
-      <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=Peralta&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-      <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-      <header>
-          <nav class="navbar" id="nav">
-              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Amazing Team Profile</span>
-          </nav>
-      </header>
-      <main>
-          <section class="container">
-              <div class="row justify-content-center" id="team-cards">
-                  <!--Cards for each profile created-->
-                  ${teamCards}
-              </div>
-          </section>
-      </main>
-  </body>
-  </html>
-`
 };
 
 module.exports = displayTeamPage;
