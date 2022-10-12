72147b1f33bb33ab013dd3efe06a575e9233cd89
Diana Vu <aznalive@hotmail.com>
2022-10-12 01:16:29 -0400

    fixed up intern test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -2,25 +2,18 @@ const Intern = require('../lib/Intern')
 
 // creating intern object
 describe('intern', () => {
-it('creates an intern object', () => {
-  const intern = new Intern('Diana', 6, 'diana.vw@gmail', 'UOFT');
-  expect(intern.school).toEqual(expect.any(String));
-})
-});
-//school of intern
-describe('school', () => {
 it('gets intern school', () => {
-  const intern = new Intern('Diana', 6, 'diana.vw@gmail', 'UOFT');
-  expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+  const school = 'uoft'
+  const intern = new Intern('Diana', 6, 'diana.vw@gmail', school);
+  expect(intern.getSchool()).toEqual(school);
 })
-});
 
-//intern role
-describe('intern role', () => {
 it('gets role of the intern', () => {
-  const intern = new Intern('Diana', 6, 'diana.vw@gmail', 'UOFT')
-
-  expect(intern.getRole()).toEqual('Intern')
+  const intern = new Intern('Diana', 6, 'diana.vw@gmail', 'Intern');
+  expect(intern.getRole()).toEqual('Intern');
 })
 });
+
+
+
 
